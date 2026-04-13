e6ba16d71d3d9f93b67a5b95a544149e6443fa0a
Natalia Tracewicz <s33507@pjwstk.edu.pl>
2026-04-13 23:40:04 +0200

    Added input validation

--- APBD_C05/DTOs/ReservationDto.cs
+++ APBD_C05/DTOs/ReservationDto.cs
@@ -1,15 +1,32 @@
-﻿using APBD_C05.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using APBD_C05.Enums;
 
 namespace APBD_C05.DTOs;
 
 public class ReservationDto
 {
+    [Required]
     public int Id { get; set; }
+
+    [Required]
     public int RoomId { get; set; }
+
+    [Required(ErrorMessage = "Organizer name is required")]
+    [MinLength(3, ErrorMessage = "Organizer name must be at least 3 characters long")]
     public string OrganizerName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Topic is required")]
     public string Topic { get; set; } = string.Empty;
-    public DateOnly Date;
-    public TimeOnly StartTime;
-    public TimeOnly EndTime;
+
+    [Required]
+    public DateOnly Date { get; set; } 
+
+    [Required]
+    public TimeOnly StartTime { get; set; }
+
+    [Required]
+    public TimeOnly EndTime { get; set; }
+
+    [Required]
     public ReservationStatus Status { get; set; }
 }
--- APBD_C05/DTOs/RoomDto.cs
+++ APBD_C05/DTOs/RoomDto.cs
@@ -1,13 +1,24 @@
-﻿namespace APBD_C05.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+namespace APBD_C05.DTOs;
 
 public class RoomDto
 {
+    [Required]
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Room name is required")]
     public string Name { get; set; } = string.Empty;
     
+    [Required(ErrorMessage = "Building code is required")]
     public string BuildingCode { get; set; } = string.Empty;
+
+    [Required]
     public int Floor { get; set; }
+
+    [Range(0.01, double.MaxValue, ErrorMessage = "Capacity must be greater than zero")]
     public double Capacity { get; set; }
+
     public bool HasProjector { get; set; }
+
     public bool IsActive { get; set; }
 }
