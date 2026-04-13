ee7cf2bbe15d4d45951de60deb3d2d0b2306747b
Natalia Tracewicz <s33507@pjwstk.edu.pl>
2026-04-13 19:03:23 +0200

    Added input data to controllers

--- APBD_C05/Controllers/ReservationsController.cs
+++ APBD_C05/Controllers/ReservationsController.cs
@@ -1,6 +1,58 @@
-﻿namespace APBD_C05.Controllers;
+﻿using APBD_C05.Enums;
+using APBD_C05.Models;
+using Microsoft.AspNetCore.Mvc;
 
+namespace APBD_C05.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
 public class ReservationsController
 {
-    
+    private static readonly List<Reservation> _reservations =
+    [
+        new()
+        {
+            Id = 1,
+            RoomId = 101,
+            OrganizerName = "John Doe",
+            Topic = "Quarterly Business Review",
+            Date = new DateOnly(2024, 6, 10),
+            StartTime = new TimeOnly(9, 0),
+            EndTime = new TimeOnly(10, 30),
+            Status = ReservationStatus.Confirmed
+        },
+        new()
+        {
+            Id = 2,
+            RoomId = 205,
+            OrganizerName = "Jane Smith",
+            Topic = "Project Kick-off: Apollo",
+            Date = new DateOnly(2024, 6, 10),
+            StartTime = new TimeOnly(11, 0),
+            EndTime = new TimeOnly(12, 0),
+            Status = ReservationStatus.Planned
+        },
+        new()
+        {
+            Id = 3,
+            RoomId = 101,
+            OrganizerName = "Michael Brown",
+            Topic = "Technical Interview - Backend Developer",
+            Date = new DateOnly(2024, 6, 11),
+            StartTime = new TimeOnly(14, 0),
+            EndTime = new TimeOnly(15, 0),
+            Status = ReservationStatus.Cancelled
+        },
+        new()
+        {
+            Id = 4,
+            RoomId = 303,
+            OrganizerName = "Emily White",
+            Topic = "Weekly Team Sync",
+            Date = new DateOnly(2024, 6, 12),
+            StartTime = new TimeOnly(10, 0),
+            EndTime = new TimeOnly(11, 0),
+            Status = ReservationStatus.Confirmed
+        }
+    ];
 }
--- APBD_C05/Controllers/RoomsController.cs
+++ APBD_C05/Controllers/RoomsController.cs
@@ -5,12 +5,12 @@ using Microsoft.AspNetCore.Mvc;
 namespace APBD_C05.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class RoomsController : ControllerBase
 {
-    public static List<Room> rooms =
+    private static readonly List<Room> _rooms =
     [
-        new Room()
+        new()
         {
             Id = 100,
             Name = "C100",
@@ -19,13 +19,49 @@ public class RoomsController : ControllerBase
             Capacity = 28.5,
             HasProjector = true,
             IsActive = true
+        }, new()
+        {
+            Id = 101,
+            Name = "C101",
+            BuildingCode = "C",
+            Floor = 1,
+            Capacity = 32,
+            HasProjector = false,
+            IsActive = true
+        }, new()
+        {
+            Id = 102,
+            Name = "A300",
+            BuildingCode = "A",
+            Floor = 3,
+            Capacity = 20.5,
+            HasProjector = false,
+            IsActive = false
+        }, new()
+        {
+            Id = 103,
+            Name = "A203",
+            BuildingCode = "A",
+            Floor = 2,
+            Capacity = 30,
+            HasProjector = true,
+            IsActive = true
+        }, new()
+        {
+            Id = 104,
+            Name = "B404",
+            BuildingCode = "B",
+            Floor = 4,
+            Capacity = 17,
+            HasProjector = false,
+            IsActive = true
         }
     ];
 
     [HttpGet] 
     public IActionResult GetAll()
     {
-        return Ok(rooms.Select(e => new RoomDto()
+        return Ok(_rooms.Select(e => new RoomDto()
         {
             Id = e.Id,
             Name = e.Name,
@@ -41,11 +77,11 @@ public class RoomsController : ControllerBase
     [Route("{id:int}")]
     public IActionResult GetById(int id)
     {
-        var room = rooms.FirstOrDefault(e => e.Id == id);
+        var room = _rooms.FirstOrDefault(e => e.Id == id);
         if (room is null)
             return NotFound($"Pokój o id {id} nie istnieje");
 
-        return Ok(rooms.Select(e => new RoomDto()
+        return Ok(_rooms.Select(e => new RoomDto()
         {
             Id = e.Id,
             Name = e.Name,
@@ -57,24 +93,25 @@ public class RoomsController : ControllerBase
         }));
     }
 
-    // [HttpGet]
-    // [Route("buildingCode/{buildingCode:string}")]
-    // public IActionResult GetRoomsFromBuildingCode(string buildingCode)
-    // {
-    //     // var code =
-    //     
-    //     return Ok(rooms.Select(e => new RoomDto()
-    //     {
-    //         Id = e.Id,
-    //         Name = e.Name,
-    //         BuildingCode = e.BuildingCode,
-    //         Floor = e.Floor,
-    //         Capacity = e.Capacity,
-    //         HasProjector = e.HasProjector,
-    //         IsActive = e.IsActive
-    //     }));
-    //     
-    // }
+    [HttpGet]
+    [Route("buildingCode/{buildingCode:string}")]
+    public IActionResult GetRoomsFromBuildingCode(string buildingCode)
+    {
+        var code = _rooms.FirstOrDefault(e => e.BuildingCode == buildingCode);
 
-
+        if (code is null)
+            return NotFound($"Budynek z kodem {buildingCode} nie istnieje");
+        return Ok(_rooms.Select(e => new RoomDto()
+        {
+            Id = e.Id,
+            Name = e.Name,
+            BuildingCode = e.BuildingCode,
+            Floor = e.Floor,
+            Capacity = e.Capacity,
+            HasProjector = e.HasProjector,
+            IsActive = e.IsActive
+        }).Where(e => e.BuildingCode == buildingCode));
+    }
+    
+    
 }
