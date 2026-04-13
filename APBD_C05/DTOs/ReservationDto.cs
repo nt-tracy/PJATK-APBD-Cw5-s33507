bd451bddfdd60cddfd8c06e3e88efff9856a4449
Natalia Tracewicz <s33507@pjwstk.edu.pl>
2026-04-13 18:07:02 +0200

    Added ReservationDto

---
+++ APBD_C05/DTOs/ReservationDto.cs
@@ -0,0 +1,15 @@
+﻿using APBD_C05.Enums;
+
+namespace APBD_C05.DTOs;
+
+public class ReservationDto
+{
+    public int Id { get; set; }
+    public int RoomId { get; set; }
+    public string OrganizerName { get; set; } = string.Empty;
+    public string Topic { get; set; } = string.Empty;
+    public DateOnly Date;
+    public TimeOnly StartTime;
+    public TimeOnly EndTime;
+    public ReservationStatus Status { get; set; }
+}
